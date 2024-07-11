a0ecfa8cbe611f149a5fed0e6162fb620b5b6da8
gabogsb <106564407+gabogsb@users.noreply.github.com>
2024-07-10 22:32:52 -0300

    setup drawerOptions

---
+++ app/src/main/java/br/com/gabodev/musicappui/Screen.kt
@@ -0,0 +1,32 @@
+package br.com.gabodev.musicappui
+
+import androidx.annotation.DrawableRes
+
+sealed class Screen(
+  val title: String,
+  val route: String
+) {
+
+  sealed class DrawerScreen(
+    val dTitle: String,
+    val dRoute: String,
+    @DrawableRes val icon: Int
+  ) : Screen(dTitle, dRoute) {
+    object Account: DrawerScreen(
+      "Account",
+      "account",
+      R.drawable.baseline_account_box_24
+    )
+    object Subscription: DrawerScreen(
+      "Subscription",
+      "subscription",
+      R.drawable.baseline_library_music_24
+    )
+    object AddAccount: DrawerScreen(
+      "Add Account",
+      "add_account",
+      R.drawable.baseline_person_add_alt_1_24
+    )
+  }
+
+}
--- app/src/main/java/br/com/gabodev/musicappui/ui/theme/MainView.kt
+++ app/src/main/java/br/com/gabodev/musicappui/ui/theme/MainView.kt
@@ -1,6 +1,11 @@
 package br.com.gabodev.musicappui.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -15,7 +20,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.gabodev.musicappui.Screen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -53,4 +62,34 @@ fun MainView(
 @Composable
 private fun PreviewMainView() {
   MainView()
+}
+
+@Composable
+fun DrawerItem(
+  selected: Boolean = false,
+  item:Screen.DrawerScreen,
+  onDrawerItemClick: () -> Unit
+) {
+
+  val background = if (selected) Color.LightGray else Color.White
+
+  Row(
+    modifier = Modifier.fillMaxWidth()
+      .padding(horizontal = 8.dp, vertical = 16.dp)
+      .background(background)
+      .clickable {
+        onDrawerItemClick()
+      }
+  ) {
+    Icon(
+      painter = painterResource(id = item.icon),
+      contentDescription = item.dTitle,
+      modifier = Modifier.padding(end = 8.dp, top = 4.dp)
+    )
+    Text(
+      text = item.dTitle,
+      style = MaterialTheme.typography.h5
+    )
+  }
+
 }
